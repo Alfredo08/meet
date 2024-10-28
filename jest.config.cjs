c3d35a645f7972b0f069ed8ec99a7f2d47913849
Alfredo08 <alfredosv08@gmail.com>
2024-10-27 21:16:49 -0600

    Testing added

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,9 +1,12 @@
 module.exports = {
-    testEnvironment: 'jest-environment-jsdom', 
-    transform: {
-      '^.+\\.jsx?$': 'babel-jest', // Transform JavaScript files using Babel
-    },
-    moduleFileExtensions: ['js', 'jsx'],
-    setupFilesAfterEnv: ['<rootDir>/setupTests.js'], // Path to the setup file
-   };
+  testEnvironment: 'jest-environment-jsdom',
+  transform: {
+    '^.+\\.jsx?$': 'babel-jest', // Transform JavaScript and JSX files using Babel
+  },
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy', // Mock CSS imports
+  },
+  moduleFileExtensions: ['js', 'jsx'],
+  setupFilesAfterEnv: ['<rootDir>/setupTests.js'], // Path to the setup file
+};
    
--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import EventList from './components/EventList';
 import CitySearch from './components/CitySearch';
+import NumberOfEvents from './components/NumberOfEvents';
 import { useEffect, useState } from 'react';
 import { extractLocations, getEvents } from './api';
-import NumberOfEvents from './components/NumberOfEvents';
 
 import './App.css';
 
@@ -12,34 +12,25 @@ const App = () => {
   const [currentNOE, setCurrentNOE] = useState(32);
   const [allLocations, setAllLocations] = useState([]);
   const [currentCity, setCurrentCity] = useState("See all cities");
-  const [errorAlert, setErrorAlert] = useState("");
 
   useEffect(() => {
     fetchData();
-  }, [currentCity, currentNOE]);
+  }, [currentCity]);
 
   const fetchData = async () => {
     const allEvents = await getEvents();
     const filteredEvents = currentCity === "See all cities" ?
-      allEvents : allEvents.filter(event => event.location === currentCity)
+      allEvents :
+      allEvents.filter(event => event.location === currentCity)
     setEvents(filteredEvents.slice(0, currentNOE));
     setAllLocations(extractLocations(allEvents));
   }
-  
+
   return (
     <div className="App">
-      <CitySearch 
-        allLocations={allLocations} 
-        setCurrentCity={setCurrentCity} 
-      />
-      <EventList 
-        events={events} 
-      />
-      <NumberOfEvents 
-        setErrorAlert={setErrorAlert}
-        currentNOE={currentNOE}
-        setCurrentNOE={setCurrentNOE}
-      />
+      <CitySearch allLocations={allLocations} setCurrentCity={setCurrentCity}/>
+      <NumberOfEvents />
+      <EventList events={events}/>
     </div>
   );
 }
