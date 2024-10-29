c36f524b575fbfb3185af6e050b014911bafbcdc
Alfredo08 <alfredosv08@gmail.com>
2024-10-29 08:58:41 -0600

    Finishing end to end testing

--- src/components/Event.jsx
+++ src/components/Event.jsx
@@ -6,7 +6,7 @@ import { useState } from "react";
 const Event = ({ event }) => {
   const [showDetails, setShowDetails] = useState(false);
   return (
-    <li>
+    <li className="event">
       <div className="eventSummary">
         <h2>{event.summary}</h2>
         <p>{event.location}</p>
